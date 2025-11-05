859226f258430ec1645d8a4e94cc21a23d2da1e8
CodeByVarun <142214068+HackStreetBoy11@users.noreply.github.com>
2025-11-05 11:46:52 +0530

    Create shortestPathInBinaryMatrix.cpp

---
+++ Graphs/shortestPathInBinaryMatrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    typedef pair<int,pair<int,int>> P;
+   int dr[8] = {-1,-1,-1,0,1,1,1,0};
+int dc[8] = {-1,0,1,1,1,0,-1,-1};
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m = grid[0].size();
+        if(grid[0][0]==1 || grid[n-1][m-1]==1)return -1;
+        queue<P> q;
+        q.push({1,{0,0}});// source
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        visited[0][0]=1;
+        while(!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+            int dis = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
+            if(r==n-1 && c==m-1)
+            {
+                return dis;
+            }
+            for(int i=0;i<8;i++)    
+            {
+                    int nrow = dr[i]+r;
+                    int ncol = dc[i]+c;
+
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==0 && visited[nrow][ncol]!=1)
+                    {
+                        q.push({dis+1,{nrow,ncol}});
+                        visited[nrow][ncol]=1;
+                    }
+
+            }
+
+        }
+        return -1;
+
+    }
+};
