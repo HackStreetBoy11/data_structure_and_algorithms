1c91c9bab4cf387ba47d8bd983b1bfad1b43d7ae
CodeByVarun <142214068+HackStreetBoy11@users.noreply.github.com>
2025-11-10 10:15:26 +0530

    Create validateBinarySearchTree.cpp

---
+++ Trees/validateBinarySearchTree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool fun(TreeNode* root,long long low,long long high)
+    {
+        if(root==nullptr)return true;
+        if(root->val <= low || root->val>= high)return false;
+        return fun(root->left,low,root->val)&& fun(root->right,root->val,high);
+    }
+    bool isValidBST(TreeNode* root) {
+        // intution is based on range
+        return fun(root,LLONG_MIN,LLONG_MAX);
+    }
+};
