30d2c7b9224fcd0ea5fd44581566321b3b163f14
CodeByVarun <142214068+HackStreetBoy11@users.noreply.github.com>
2025-11-14 11:50:54 +0530

    Implement in-place permutation generation

---
+++ Recursion/permutationInPlace.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+
+    void fun(vector<int>& nums,int idx)
+    {
+        if(idx==nums.size())
+        {
+            result.push_back(nums);
+            return;
+        }
+        for(int i=idx;i<nums.size();i++)
+        {
+            swap(nums[idx],nums[i]);
+            fun(nums,idx+1);
+            swap(nums[idx],nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+    fun(nums,0);
+    return result;
+    }
+};
