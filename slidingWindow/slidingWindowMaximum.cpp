c96d14d58ed5ce87a0f8f5ad1d74942385980209
CodeByVarun <142214068+HackStreetBoy11@users.noreply.github.com>
2025-11-18 16:52:14 +0530

    Implement sliding window maximum algorithm

---
+++ slidingWindow/slidingWindowMaximum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        // dq.front() value jab window valid hai tab
+        int l=0,r=0;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            while(!dq.empty() && dq.front()<=i-k)
+            {
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[i]>nums[dq.back()])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i>=k-1)
+            {
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
